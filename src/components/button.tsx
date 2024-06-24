b5a0e6f614b4ea0fe8bd24f6ded5d123ea16a872
kayla williams <shalisha.angelique@gmail.com>
2024-06-23 23:36:34 -0400

    Create button component

---
+++ src/components/button.tsx
@@ -0,0 +1,17 @@
+import { Link } from "gatsby";
+import styled from "styled-components";
+
+export const Button = styled(Link)`
+    background-color: #BFC0C0;
+    border: none;
+    border-radius: 5px;
+    padding: 5px;
+    color: #003B36;
+    font-size: 20px;
+    margin: 12px 0 25px;
+    width: fit-content;
+    display: flex;
+    justify-self: center;
+    justify-content: center;
+    text-decoration: none;
+`;
