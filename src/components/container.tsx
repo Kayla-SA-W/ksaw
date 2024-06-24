3750307974762799e116c348ba14a01eb9505f70
kayla williams <shalisha.angelique@gmail.com>
2024-06-23 23:36:26 -0400

    Create container component

---
+++ src/components/container.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: fit-content;
+    width: fit-content;
+    background-color: #003B36;
+    display: grid;
+    place-self: center;
+`;
