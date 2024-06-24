d61eb48cccc988f323c58bcd1a0e86c8ec5c63ff
kayla williams <shalisha.angelique@gmail.com>
2024-06-23 23:33:31 -0400

    Add file types for typescript

---
+++ src/types/images.d.ts
@@ -0,0 +1,9 @@
+declare module '*.svg' {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.png';
+  declare module '*.jpeg';
+  declare module '*.jpg';
+  declare module '*.pdf';
