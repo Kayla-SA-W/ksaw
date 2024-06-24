735d658cbad26e5e8fd78181ec4ff8a9b4b13c2c
kayla williams <shalisha.angelique@gmail.com>
2024-06-23 23:37:32 -0400

    Redo 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,24 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
+import React from "react";
+import { GlobalContainer } from "../components/global-container";
+import { Button } from "../components/button";
+import styled from "styled-components";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const NotFoundcopy = styled.p`
+padding: 5px;
+    color: #003B36;
+    font-size: 20px;
+`;
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+const NotFound = () => (
+  <GlobalContainer>
+    <div>
+    <NotFoundcopy>
+      The Page your looking for does not exist.
+      Please return home.
+    </NotFoundcopy>
+    <Button to="/">Home</Button>
+    </div>
+  </GlobalContainer>
+);
 
-const NotFoundPage: React.FC<PageProps> = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
-
-export default NotFoundPage
-
-export const Head: HeadFC = () => <title>Not found</title>
+export default NotFound;
